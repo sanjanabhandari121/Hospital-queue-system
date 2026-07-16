222fbbe7eb91cabaf3cbf63fe986e18937cda914
sanjanabhandari121 <sanjanabhandari7474@gmail.com>
2026-07-16 21:33:33 +0530

    Fix CORS origin

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const server=http.createServer(app);
 app.use(cors({
     origin:[
         "http://localhost:5173",
-        "https://hospital-queue-system.vercel.app"
+        "https://hospital-queue-system-one.vercel.app"
     ],
     credentials:true
 }));
@@ -30,7 +30,7 @@ const io=new Server(server,{
     cors:{
         origin:[
             "http://localhost:5173",
-            "https://hospital-queue-system.vercel.app"
+            "https://hospital-queue-system-one.vercel.app"
         ],
         methods:["GET","POST","PUT","DELETE"],
         credentials:true
